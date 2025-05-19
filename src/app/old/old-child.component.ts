ae1dbcba3a2cd7a8817e179096faa91ec0359c0f
Pascal Klaiss <pascal.klaiss@smino.com>
2025-05-19 15:26:40 +0200

    wip

--- src/app/old/old-child.component.ts
+++ src/app/old/old-child.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-old-child',
   imports: [],
   template: `
-    <p>
-      old-child works!
-    </p>
+    <div>
+      Child {{ count }}
+      <button (click)="onClick()">Click {{ click }}</button>
+    </div>
+    @if (click > 0) {
+      <app-old-child></app-old-child>
+    }
   `,
   styles: ``
 })
 export class OldChildComponent {
+  private render = 1;
 
+  get count() {
+    return this.render++;
+  }
+
+  protected click = 0;
+
+  onClick() {
+    this.click++;
+  }
 }
--- src/app/old/old-container.component.ts
+++ src/app/old/old-container.component.ts
@@ -1,14 +1,18 @@
 import {Component, Input, input, OnChanges} from '@angular/core';
+import {OldChildComponent} from './old-child.component';
 
 @Component({
   selector: 'app-old-container',
-  imports: [],
+  imports: [
+    OldChildComponent
+  ],
   template: `
     <div style="border: 2px solid greenyellow">
-      Signal <button (dblclick)="ondblclick()">test</button>
+      Old <button (dblclick)="ondblclick()">test</button>
       <hr>
       <p> {{ name }} &nbsp;{{ count }}</p>
       <hr>
+      <app-old-child></app-old-child>
     </div>
     <ng-content></ng-content>
   `,
